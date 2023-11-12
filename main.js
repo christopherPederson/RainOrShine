e1181985ca56a17b422f185909dd4babee2a1821
christopherPederson <127063705+christopherPederson@users.noreply.github.com>
2023-11-12 15:43:04 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
-marqueeArray = new Array(10)
-cityCodeArray = new Array(10)
-cityLongitudeArray = new Array(10)
-cityLatitudeArray = new Array(10)
-cityNamesArray = [
+///////////////////////////////Veriable Decleration///////////////////////////////
+cityNamesArray = [ // City Decleration, number of cities determines the number of marquee items
     "montreal",
     "toronto",
     "vancouver",
@@ -14,9 +11,16 @@ cityNamesArray = [
     "halifax",
     "saskatoon",
 ]
+marqueeArray = new Array(cityNamesArray.length)
+cityCodeArray = new Array(cityNamesArray.length)
+cityLongitudeArray = new Array(cityNamesArray.length)
+cityLatitudeArray = new Array(cityNamesArray.length)
 
 let marqueeWrapper = document.querySelector(".marquee__textWrapper");
 let hiddenMarqueeWrapper = document.querySelector(".marquee__textWrapper--hidden");
+
+///////////////////////////////Veriable Decleration///////////////////////////////
+///////////////////////////////Function Decleration///////////////////////////////
 
 let getCityCoordinates = (_cityNamesArray, _cityLatitudeArray, _cityLongitudeArray) => {
 
@@ -30,7 +34,7 @@ let getCityCoordinates = (_cityNamesArray, _cityLatitudeArray, _cityLongitudeArr
 
         }).then((data) => {
 
-            if (data.results[0].country == 'Canada'){
+            if (data.results[0].country === 'Canada'){
                     _cityLongitudeArray[i] = data.results[0].longitude
                     _cityLatitudeArray[i] = data.results[0].latitude
             }else{
@@ -59,6 +63,64 @@ let getCodeData = (_cityNamesArray, _cityCodeArray, _cityLatitudeArray, _cityLon
         })
     }
 };
+let constructMarqueeStrings = (_cityNamesArray, _cityCodeArray, _marqueeArray) => {
+
+    let weatherSymbol = "";
+
+    for(let i = 0; i < _cityNamesArray.length; i++){
+
+        if (_cityCodeArray[i] === 0 || 
+            _cityCodeArray[i] === 1){
+                //sunny
+                weatherSymbol = "&#x2600;";
+        }
+        else if (_cityCodeArray[i] === 2 || 
+                _cityCodeArray[i] === 3){
+
+                    //cloudy
+                    weatherSymbol = "&#x1F324;";
+        }
+        else if (_cityCodeArray[i] === 51 || 
+                _cityCodeArray[i] === 53 ||
+                _cityCodeArray[i] === 55 ||
+                _cityCodeArray[i] === 56 ||
+                _cityCodeArray[i] === 57 ||
+                _cityCodeArray[i] === 61 ||
+                _cityCodeArray[i] === 63 ||
+                _cityCodeArray[i] === 65 ||
+                _cityCodeArray[i] === 66 ||
+                _cityCodeArray[i] === 67 ||
+                _cityCodeArray[i] === 80 ||
+                _cityCodeArray[i] === 81 ||
+                _cityCodeArray[i] === 82){
+
+                    //rainy
+                    weatherSymbol = "&#x1F327;";
+        }
+        else if (_cityCodeArray[i] === 71 || 
+                _cityCodeArray[i] === 73 ||
+                _cityCodeArray[i] === 75 ||
+                _cityCodeArray[i] === 77 ||
+                _cityCodeArray[i] === 85 ||
+                _cityCodeArray[i] === 86){
+
+                    //snow
+                    weatherSymbol = "&#x26C7;";
+        }
+        else if (_cityCodeArray[i] === 95 || 
+            _cityCodeArray[i] === 96 ||
+            _cityCodeArray[i] === 99){
+
+                //storm
+                weatherSymbol = "&#x1F329;";
+        }
+        else{
+            // no data
+            weatherSymbol = "&#x1F6AB;";
+        }
+    }
+        
+}
 let populateMarquee = (array, itemWrapper, hiddenItemWrapper) => {
     for(let i = 0; i < array.length; i++){
         itemWrapper.innerHTML += `<div class="marquee__item--num${i + 1} marquee__item">${array[i]}</div>`;
@@ -68,4 +130,11 @@ let populateMarquee = (array, itemWrapper, hiddenItemWrapper) => {
         }
 };
 
+///////////////////////////////Function Decleration///////////////////////////////
+///////////////////////////////Function Calls///////////////////////////////
+
+getCityCoordinates(cityNamesArray, cityLatitudeArray, cityLongitudeArray);
+getCodeData(cityNamesArray, cityCodeArray, cityLatitudeArray, cityLongitudeArray);
 populateMarquee(marqueeArray, marqueeWrapper, hiddenMarqueeWrapper);
+
+///////////////////////////////Function Calls///////////////////////////////
