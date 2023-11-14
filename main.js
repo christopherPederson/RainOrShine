bb4db0ca7aeeaa9aadf5ecc0dd611bddf6e45d72
christopherPederson <127063705+christopherPederson@users.noreply.github.com>
2023-11-13 22:53:00 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+///////////////////////////////Dev Notes///////////////////////////////
+// Christopher Pederson-2023
+// this wether app is desighned to take advantage of the open-meteo api to 
+//display the weather for a number of cities, features will eventually inclue
+//a live data stream marquee, data visualization of any supported city, and
+//a search function to find cities with auto fill features
+
+// currently non functional, marquee elements need to be loaded to the marquee divs
+///////////////////////////////Dev Notes///////////////////////////////
 ///////////////////////////////Veriable Decleration///////////////////////////////
 cityNamesArray = [ // City Decleration, number of cities determines the number of marquee items
     "montreal",
@@ -11,40 +20,61 @@ cityNamesArray = [ // City Decleration, number of cities determines the number o
     "halifax",
     "saskatoon",
 ]
-marqueeArray = new Array(cityNamesArray.length)
-cityCodeArray = new Array(cityNamesArray.length)
-cityLongitudeArray = new Array(cityNamesArray.length)
-cityLatitudeArray = new Array(cityNamesArray.length)
+marqueeArray = new Array(cityNamesArray.length)//array to hold the marquee strings
+cityCodeArray = new Array(cityNamesArray.length)//array to hold the city codes specific codes are used to determine the weather symbol
+cityLongitudeArray = new Array(cityNamesArray.length)//array to hold the corresponding city longitudes
+cityLatitudeArray = new Array(cityNamesArray.length)//array to hold the corresponding city latitudes
 
-let marqueeWrapper = document.querySelector(".marquee__textWrapper");
-let hiddenMarqueeWrapper = document.querySelector(".marquee__textWrapper--hidden");
+let marqueeWrapper = document.querySelector(".marquee__textWrapper");// visible marquee wrapper
+let hiddenMarqueeWrapper = document.querySelector(".marquee__textWrapper--hidden"); //hidden marquee wrapper, this acts as the second half of the marquee length to facillitate the scroling effect
 
 ///////////////////////////////Veriable Decleration///////////////////////////////
 ///////////////////////////////Function Decleration///////////////////////////////
 
-let getCityCoordinates = (_cityNamesArray, _cityLatitudeArray, _cityLongitudeArray) => {
+let populateCityCoordinates = (_cityNamesArray, _cityLatitudeArray, _cityLongitudeArray) => {
+// this function populates the city coordinates arrays with the corresponding city coordinates 
+//retrieved from the api using the getCityCoordinatesData function
 
     for(let i = 0; i < _cityNamesArray.length; i++){
+        //loop through the city names array and call the getCityCoordinatesData function for each city
+
+        getCityCoordinatesData(_cityNamesArray, i).then((data) => {
+            //succesful fetch
+
+            //test to see if the server responded with a valid city ie inside canada if not we will use the default city
+        }, (error) => {
+            //failed fetch
+            console.log(error)
+            //call server not responding function
+        })
+    }
+}
+let getCityCoordinatesData = (_cityNamesArray, i) => {
+// returns the raw json data from the api for the corresponding city name
+// the data is then used to populate the city coordinates arrays
+// the function is desighned to be used while iterating through the city names array
+    return new Promise((resolve, reject) => {
 
         fetch(`https://geocoding-api.open-meteo.com/v1/search?name=
-            ${_cityNamesArray[i]}&count=1&language=en&format=json`
-        ).then((response) => {
+        ${_cityNamesArray[i]}&count=1&language=en&format=json`)
+        .then((response) => {
 
             return response.json();
 
         }).then((data) => {
 
-            if (data.results[0].country === 'Canada'){
-                    _cityLongitudeArray[i] = data.results[0].longitude
-                    _cityLatitudeArray[i] = data.results[0].latitude
-            }else{
-                console.log(`bad query input ${_cityNamesArray[i]}`)
-            }
+            resolve(data)
 
+        }).catch(() => {
+
+            reject(error)
+            
         })
-    }
-}
+    })
+};
 let getCodeData = (_cityNamesArray, _cityCodeArray, _cityLatitudeArray, _cityLongitudeArray) => {
+// this function populates the city code array with the corresponding city codes for each city string
+// this function is reliant on the latitude and longitude arrays being populated first
     for(let i = 0; i < _cityNamesArray.length; i++){
 
         fetch(`https://api.open-meteo.com/v1/forecast?
@@ -65,6 +95,7 @@ let getCodeData = (_cityNamesArray, _cityCodeArray, _cityLatitudeArray, _cityLon
 };
 let constructMarqueeStrings = (_cityNamesArray, _cityCodeArray, _marqueeArray) => {
 
+    // this function translates the wether codes provided by the API into visual elements for the marquee
     let weatherSymbol = "";
 
     for(let i = 0; i < _cityNamesArray.length; i++){
@@ -120,8 +151,9 @@ let constructMarqueeStrings = (_cityNamesArray, _cityCodeArray, _marqueeArray) =
         }
     }
         
-}
+};
 let populateMarquee = (array, itemWrapper, hiddenItemWrapper) => {
+    // populates both the hidden and visible marquee wrappers with the marquee items
     for(let i = 0; i < array.length; i++){
         itemWrapper.innerHTML += `<div class="marquee__item--num${i + 1} marquee__item">${array[i]}</div>`;
         }
@@ -133,7 +165,7 @@ let populateMarquee = (array, itemWrapper, hiddenItemWrapper) => {
 ///////////////////////////////Function Decleration///////////////////////////////
 ///////////////////////////////Function Calls///////////////////////////////
 
-getCityCoordinates(cityNamesArray, cityLatitudeArray, cityLongitudeArray);
+populateCityCoordinates(cityNamesArray, cityLatitudeArray, cityLongitudeArray);
 getCodeData(cityNamesArray, cityCodeArray, cityLatitudeArray, cityLongitudeArray);
 populateMarquee(marqueeArray, marqueeWrapper, hiddenMarqueeWrapper);
 
